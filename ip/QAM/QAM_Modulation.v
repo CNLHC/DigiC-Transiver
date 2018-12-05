2714fa421adb98d0bca58ec679c8f4e7d88f6c52
CNLHC <2463765697@qq.com>
2018-12-05 21:47:09 +0800

    为QAM Module添加Burst模式需要的接口
    此外，移除了QSys中的冗余项（包括HPS/Pattern Generator 和 Pattern Checker)

--- ip/QAM/QAM_Modulation.v
+++ ip/QAM/QAM_Modulation.v
@@ -12,17 +12,19 @@
 module QAM_Modulation #(
 		parameter PACKET_LENGTH = 1024
 	) (
-		input  wire [31:0] asi_in0_data,   //  asi_in0.data
-		output wire        asi_in0_ready,  //         .ready
-		input  wire        asi_in0_valid,  //         .valid
-		input  wire        clock_clk,      //    clock.clk
-		input  wire        reset_reset,    //    reset.reset
-		output wire [31:0] aso_out0_data,  // aso_out0.data
-		input  wire        aso_out0_ready, //         .ready
-		output wire        aso_out0_valid, //         .valid
-		output wire [31:0] aso_out1_data,  // aso_out1.data
-		input  wire        aso_out1_ready, //         .ready
-		output wire        aso_out1_valid  //         .valid
+		input  wire [31:0] asi_in0_data,          //  asi_in0.data
+		output wire        asi_in0_ready,         //         .ready
+		input  wire        asi_in0_valid,         //         .valid
+		input  wire        asi_in0_startofpacket, //         .startofpacket
+		input  wire        asi_in0_endofpacket,   //         .endofpacket
+		input  wire        clock_clk,             //    clock.clk
+		input  wire        reset_reset,           //    reset.reset
+		output wire [31:0] aso_out0_data,         // aso_out0.data
+		input  wire        aso_out0_ready,        //         .ready
+		output wire        aso_out0_valid,        //         .valid
+		output wire [31:0] aso_out1_data,         // aso_out1.data
+		input  wire        aso_out1_ready,        //         .ready
+		output wire        aso_out1_valid         //         .valid
 	);
 
 	// TODO: Auto-generated HDL template
