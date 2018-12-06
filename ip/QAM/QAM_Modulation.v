f9342f8485a8ed7ed2a3da54a70ce0805e1274ba
CNLHC <2463765697@qq.com>
2018-12-06 17:05:45 +0800

    修改QAM模块的定义，硬编码以适应后端FFT模块

--- ip/QAM/QAM_Modulation.v
+++ ip/QAM/QAM_Modulation.v
@@ -28,15 +28,51 @@ module QAM_Modulation #(
 	);
 
 	// TODO: Auto-generated HDL template
+   
 
-	assign asi_in0_ready = 1'b0;
+	//assign asi_in0_ready = 1'b0;
+	//assign aso_out0_valid = 1'b0;
+	//assign aso_out0_data = 32'b00000000000000000000000000000000;
+	//assign aso_out1_valid = 1'b0;
+	//assign aso_out1_data = 32'b00000000000000000000000000000000;
+    assign  asi_in0_ready= 1'b1;
 
-	assign aso_out0_valid = 1'b0;
-
-	assign aso_out0_data = 32'b00000000000000000000000000000000;
-
-	assign aso_out1_valid = 1'b0;
-
-	assign aso_out1_data = 32'b00000000000000000000000000000000;
-
+    integer index;
+    reg [1:0]tInnerStateFlag;//00-idle 01-reading packet
+    always @(posedge asi_in0_startofpacket) begin
+        case tInnerStateFlag:
+            2'b00: tInnerStateFlag<=2'b01;
+        endcase
+    end
+    always @(posedge clock_clk) begin
+        case tInnerStateFlag:
+            2'b01:
+            if(asi_in0_valid) begin 
+                for (index=1;index<=16;index=index+1) begin
+                    case asi_in0_data[2*index-1:2*(index-1)] 
+                        2'b00: 
+                            begin 
+                                aso_out0_data[2*index-1:2*(index-1)]<=1;
+                                aso_out1_data[2*index-1:2*(index-1)]<=1;
+                            end
+                        2'b01:
+                            begin 
+                                aso_out0_data[2*index-1:2*(index-1)]<=-1;
+                                aso_out1_data[2*index-1:2*(index-1)]<=1;
+                            end
+                        2'b11:
+                            begin 
+                                aso_out0_data[2*index-1:2*(index-1)]<=-1;
+                                aso_out1_data[2*index-1:2*(index-1)]<=-1;
+                            end
+                        2'b10:
+                            begin 
+                                aso_out0_data[2*index-1:2*(index-1)]<=1;
+                                aso_out1_data[2*index-1:2*(index-1)]<=-1;
+                            end
+                    endcase
+                end
+            end
+        endcase
+    end
 endmodule
