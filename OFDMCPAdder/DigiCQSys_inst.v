55ce8da17585efcc559bd4188894fee74377d1f9
CNLHC <2463765697@qq.com>
2018-12-17 23:28:59 +0800

    重新组织了代码结构，将Transmitter和Receiver并入一个QSys

---
+++ OFDMCPAdder/DigiCQSys_inst.v
@@ -0,0 +1,15 @@
+	DigiCQSys u0 (
+		.global_reset_reset_n                   (<connected-to-global_reset_reset_n>),                   //                   global_reset.reset_n
+		.ofdm_dac_control_0_dac_control_chadata (<connected-to-ofdm_dac_control_0_dac_control_chadata>), // ofdm_dac_control_0_dac_control.chadata
+		.ofdm_dac_control_0_dac_control_chbdata (<connected-to-ofdm_dac_control_0_dac_control_chbdata>), //                               .chbdata
+		.pll_0_locked_export                    (<connected-to-pll_0_locked_export>),                    //                   pll_0_locked.export
+		.pll_0_refclk_clk                       (<connected-to-pll_0_refclk_clk>),                       //                   pll_0_refclk.clk
+		.qsys_clkin_clk                         (<connected-to-qsys_clkin_clk>),                         //                     qsys_clkin.clk
+		.sc_fifo_0_clk_reset_reset              (<connected-to-sc_fifo_0_clk_reset_reset>),              //            sc_fifo_0_clk_reset.reset
+		.signal_tap_clock_clk                   (<connected-to-signal_tap_clock_clk>),                   //               signal_tap_clock.clk
+		.spislave_0_export_0_mosi               (<connected-to-spislave_0_export_0_mosi>),               //            spislave_0_export_0.mosi
+		.spislave_0_export_0_nss                (<connected-to-spislave_0_export_0_nss>),                //                               .nss
+		.spislave_0_export_0_miso               (<connected-to-spislave_0_export_0_miso>),               //                               .miso
+		.spislave_0_export_0_sclk               (<connected-to-spislave_0_export_0_sclk>)                //                               .sclk
+	);
+
