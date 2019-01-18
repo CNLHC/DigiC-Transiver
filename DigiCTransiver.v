785bc0fbd5f2b2b782f28354aa2b7849c08dd2b3
CNLHC <2463765697@qq.com>
2019-01-18 23:51:00 +0800

    微调QSYS内部内容

--- DigiCTransiver.v
+++ DigiCTransiver.v
@@ -161,117 +161,108 @@ module DigiCTransiver(
     wire globalReset;
     assign globalReset=KEY[0];
     wire signalTapPLL;
+    wire [1:0]Hps2FPGAReset;
 
 
 
 
 	soc_system u0 (
-
-		.memory_mem_a                          ( HPS_DDR3_ADDR),                       //                memory.mem_a
-		.memory_mem_ba                         ( HPS_DDR3_BA),                         //                .mem_ba
-		.memory_mem_ck                         ( HPS_DDR3_CK_P),                       //                .mem_ck
-		.memory_mem_ck_n                       ( HPS_DDR3_CK_N),                       //                .mem_ck_n
-		.memory_mem_cke                        ( HPS_DDR3_CKE),                        //                .mem_cke
-		.memory_mem_cs_n                       ( HPS_DDR3_CS_N),                       //                .mem_cs_n
-		.memory_mem_ras_n                      ( HPS_DDR3_RAS_N),                      //                .mem_ras_n
-		.memory_mem_cas_n                      ( HPS_DDR3_CAS_N),                      //                .mem_cas_n
-		.memory_mem_we_n                       ( HPS_DDR3_WE_N),                       //                .mem_we_n
-		.memory_mem_reset_n                    ( HPS_DDR3_RESET_N),                    //                .mem_reset_n
-		.memory_mem_dq                         ( HPS_DDR3_DQ),                         //                .mem_dq
-		.memory_mem_dqs                        ( HPS_DDR3_DQS_P),                      //                .mem_dqs
-		.memory_mem_dqs_n                      ( HPS_DDR3_DQS_N),                      //                .mem_dqs_n
-		.memory_mem_odt                        ( HPS_DDR3_ODT),                        //                .mem_odt
-		.memory_mem_dm                         ( HPS_DDR3_DM),                         //                .mem_dm
-		.memory_oct_rzqin                      ( HPS_DDR3_RZQ),                        //                .oct_rzqin
-		//HPS ethernet		
-		.hps_0_hps_io_hps_io_emac1_inst_TX_CLK ( HPS_ENET_GTX_CLK),       //                             hps_0_hps_io.hps_io_emac1_inst_TX_CLK
-		.hps_0_hps_io_hps_io_emac1_inst_TXD0   ( HPS_ENET_TX_DATA[0] ),   //                             .hps_io_emac1_inst_TXD0
-		.hps_0_hps_io_hps_io_emac1_inst_TXD1   ( HPS_ENET_TX_DATA[1] ),   //                             .hps_io_emac1_inst_TXD1
-		.hps_0_hps_io_hps_io_emac1_inst_TXD2   ( HPS_ENET_TX_DATA[2] ),   //                             .hps_io_emac1_inst_TXD2
-		.hps_0_hps_io_hps_io_emac1_inst_TXD3   ( HPS_ENET_TX_DATA[3] ),   //                             .hps_io_emac1_inst_TXD3
-		.hps_0_hps_io_hps_io_emac1_inst_RXD0   ( HPS_ENET_RX_DATA[0] ),   //                             .hps_io_emac1_inst_RXD0
-		.hps_0_hps_io_hps_io_emac1_inst_MDIO   ( HPS_ENET_MDIO ),         //                             .hps_io_emac1_inst_MDIO
-		.hps_0_hps_io_hps_io_emac1_inst_MDC    ( HPS_ENET_MDC  ),         //                             .hps_io_emac1_inst_MDC
-		.hps_0_hps_io_hps_io_emac1_inst_RX_CTL ( HPS_ENET_RX_DV),         //                             .hps_io_emac1_inst_RX_CTL
-		.hps_0_hps_io_hps_io_emac1_inst_TX_CTL ( HPS_ENET_TX_EN),         //                             .hps_io_emac1_inst_TX_CTL
-		.hps_0_hps_io_hps_io_emac1_inst_RX_CLK ( HPS_ENET_RX_CLK),        //                             .hps_io_emac1_inst_RX_CLK
-		.hps_0_hps_io_hps_io_emac1_inst_RXD1   ( HPS_ENET_RX_DATA[1] ),   //                             .hps_io_emac1_inst_RXD1
-		.hps_0_hps_io_hps_io_emac1_inst_RXD2   ( HPS_ENET_RX_DATA[2] ),   //                             .hps_io_emac1_inst_RXD2
-		.hps_0_hps_io_hps_io_emac1_inst_RXD3   ( HPS_ENET_RX_DATA[3] ),   //                             .hps_io_emac1_inst_RXD3
-		//HPS QSPI  
-		.hps_0_hps_io_hps_io_qspi_inst_IO0     ( HPS_FLASH_DATA[0]    ),     //                               .hps_io_qspi_inst_IO0
-		.hps_0_hps_io_hps_io_qspi_inst_IO1     ( HPS_FLASH_DATA[1]    ),     //                               .hps_io_qspi_inst_IO1
-		.hps_0_hps_io_hps_io_qspi_inst_IO2     ( HPS_FLASH_DATA[2]    ),     //                               .hps_io_qspi_inst_IO2
-		.hps_0_hps_io_hps_io_qspi_inst_IO3     ( HPS_FLASH_DATA[3]    ),     //                               .hps_io_qspi_inst_IO3
-		.hps_0_hps_io_hps_io_qspi_inst_SS0     ( HPS_FLASH_NCSO    ),        //                               .hps_io_qspi_inst_SS0
-		.hps_0_hps_io_hps_io_qspi_inst_CLK     ( HPS_FLASH_DCLK    ),        //                               .hps_io_qspi_inst_CLK
-		//HPS SD card 
-		.hps_0_hps_io_hps_io_sdio_inst_CMD     ( HPS_SD_CMD    ),           //                               .hps_io_sdio_inst_CMD
-		.hps_0_hps_io_hps_io_sdio_inst_D0      ( HPS_SD_DATA[0]     ),      //                               .hps_io_sdio_inst_D0
-		.hps_0_hps_io_hps_io_sdio_inst_D1      ( HPS_SD_DATA[1]     ),      //                               .hps_io_sdio_inst_D1
-		.hps_0_hps_io_hps_io_sdio_inst_CLK     ( HPS_SD_CLK   ),            //                               .hps_io_sdio_inst_CLK
-		.hps_0_hps_io_hps_io_sdio_inst_D2      ( HPS_SD_DATA[2]     ),      //                               .hps_io_sdio_inst_D2
-		.hps_0_hps_io_hps_io_sdio_inst_D3      ( HPS_SD_DATA[3]     ),      //                               .hps_io_sdio_inst_D3
-		//HPS USB 		  
-		.hps_0_hps_io_hps_io_usb1_inst_D0      ( HPS_USB_DATA[0]    ),      //                               .hps_io_usb1_inst_D0
-		.hps_0_hps_io_hps_io_usb1_inst_D1      ( HPS_USB_DATA[1]    ),      //                               .hps_io_usb1_inst_D1
-		.hps_0_hps_io_hps_io_usb1_inst_D2      ( HPS_USB_DATA[2]    ),      //                               .hps_io_usb1_inst_D2
-		.hps_0_hps_io_hps_io_usb1_inst_D3      ( HPS_USB_DATA[3]    ),      //                               .hps_io_usb1_inst_D3
-		.hps_0_hps_io_hps_io_usb1_inst_D4      ( HPS_USB_DATA[4]    ),      //                               .hps_io_usb1_inst_D4
-		.hps_0_hps_io_hps_io_usb1_inst_D5      ( HPS_USB_DATA[5]    ),      //                               .hps_io_usb1_inst_D5
-		.hps_0_hps_io_hps_io_usb1_inst_D6      ( HPS_USB_DATA[6]    ),      //                               .hps_io_usb1_inst_D6
-		.hps_0_hps_io_hps_io_usb1_inst_D7      ( HPS_USB_DATA[7]    ),      //                               .hps_io_usb1_inst_D7
-		.hps_0_hps_io_hps_io_usb1_inst_CLK     ( HPS_USB_CLKOUT    ),       //                               .hps_io_usb1_inst_CLK
-		.hps_0_hps_io_hps_io_usb1_inst_STP     ( HPS_USB_STP    ),          //                               .hps_io_usb1_inst_STP
-		.hps_0_hps_io_hps_io_usb1_inst_DIR     ( HPS_USB_DIR    ),          //                               .hps_io_usb1_inst_DIR
-		.hps_0_hps_io_hps_io_usb1_inst_NXT     ( HPS_USB_NXT    ),          //                               .hps_io_usb1_inst_NXT
-
-		//HPS SPI0->LCDM 	
-		.hps_0_hps_io_hps_io_spim0_inst_CLK    ( HPS_LCM_SPIM_CLK),    //                               .hps_io_spim0_inst_CLK
-		.hps_0_hps_io_hps_io_spim0_inst_MOSI   ( HPS_LCM_SPIM_MOSI),   //                               .hps_io_spim0_inst_MOSI
-		.hps_0_hps_io_hps_io_spim0_inst_MISO   ( HPS_LCM_SPIM_MISO),   //                               .hps_io_spim0_inst_MISO
-		.hps_0_hps_io_hps_io_spim0_inst_SS0    ( HPS_LCM_SPIM_SS),    //                               .hps_io_spim0_inst_SS0
-		//HPS SPI1 		  
-		.hps_0_hps_io_hps_io_spim1_inst_CLK    ( HPS_SPIM_CLK  ),           //                               .hps_io_spim1_inst_CLK
-		.hps_0_hps_io_hps_io_spim1_inst_MOSI   ( HPS_SPIM_MOSI ),           //                               .hps_io_spim1_inst_MOSI
-		.hps_0_hps_io_hps_io_spim1_inst_MISO   ( HPS_SPIM_MISO ),           //                               .hps_io_spim1_inst_MISO
-		.hps_0_hps_io_hps_io_spim1_inst_SS0    ( HPS_SPIM_SS ),             //                               .hps_io_spim1_inst_SS0
-		//HPS UART		
-		.hps_0_hps_io_hps_io_uart0_inst_RX     ( HPS_UART_RX    ),          //                               .hps_io_uart0_inst_RX
-		.hps_0_hps_io_hps_io_uart0_inst_TX     ( HPS_UART_TX    ),          //                               .hps_io_uart0_inst_TX
-		//HPS I2C1
-		.hps_0_hps_io_hps_io_i2c0_inst_SDA     ( HPS_I2C1_SDAT    ),        //                               .hps_io_i2c0_inst_SDA
-		.hps_0_hps_io_hps_io_i2c0_inst_SCL     ( HPS_I2C1_SCLK    ),        //                               .hps_io_i2c0_inst_SCL
-		//HPS I2C2
-		.hps_0_hps_io_hps_io_i2c1_inst_SDA     ( HPS_I2C2_SDAT    ),        //                               .hps_io_i2c1_inst_SDA
+		.memory_mem_a                          ( HPS_DDR3_ADDR),                       
+		.memory_mem_ba                         ( HPS_DDR3_BA),                         
+		.memory_mem_ck                         ( HPS_DDR3_CK_P),                       
+		.memory_mem_ck_n                       ( HPS_DDR3_CK_N),                       
+		.memory_mem_cke                        ( HPS_DDR3_CKE),                        
+		.memory_mem_cs_n                       ( HPS_DDR3_CS_N),                       
+		.memory_mem_ras_n                      ( HPS_DDR3_RAS_N),                      
+		.memory_mem_cas_n                      ( HPS_DDR3_CAS_N),                      
+		.memory_mem_we_n                       ( HPS_DDR3_WE_N),                       
+		.memory_mem_reset_n                    ( HPS_DDR3_RESET_N),                    
+		.memory_mem_dq                         ( HPS_DDR3_DQ),                         
+		.memory_mem_dqs                        ( HPS_DDR3_DQS_P),                      
+		.memory_mem_dqs_n                      ( HPS_DDR3_DQS_N),                      
+		.memory_mem_odt                        ( HPS_DDR3_ODT),                        
+		.memory_mem_dm                         ( HPS_DDR3_DM),                         
+		.memory_oct_rzqin                      ( HPS_DDR3_RZQ),                        
+		.hps_0_hps_io_hps_io_emac1_inst_TX_CLK ( HPS_ENET_GTX_CLK),       
+		.hps_0_hps_io_hps_io_emac1_inst_TXD0   ( HPS_ENET_TX_DATA[0] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_TXD1   ( HPS_ENET_TX_DATA[1] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_TXD2   ( HPS_ENET_TX_DATA[2] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_TXD3   ( HPS_ENET_TX_DATA[3] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_RXD0   ( HPS_ENET_RX_DATA[0] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_MDIO   ( HPS_ENET_MDIO ),         
+		.hps_0_hps_io_hps_io_emac1_inst_MDC    ( HPS_ENET_MDC  ),         
+		.hps_0_hps_io_hps_io_emac1_inst_RX_CTL ( HPS_ENET_RX_DV),         
+		.hps_0_hps_io_hps_io_emac1_inst_TX_CTL ( HPS_ENET_TX_EN),         
+		.hps_0_hps_io_hps_io_emac1_inst_RX_CLK ( HPS_ENET_RX_CLK),        
+		.hps_0_hps_io_hps_io_emac1_inst_RXD1   ( HPS_ENET_RX_DATA[1] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_RXD2   ( HPS_ENET_RX_DATA[2] ),   
+		.hps_0_hps_io_hps_io_emac1_inst_RXD3   ( HPS_ENET_RX_DATA[3] ),   
+		.hps_0_hps_io_hps_io_qspi_inst_IO0     ( HPS_FLASH_DATA[0]    ),     
+		.hps_0_hps_io_hps_io_qspi_inst_IO1     ( HPS_FLASH_DATA[1]    ),     
+		.hps_0_hps_io_hps_io_qspi_inst_IO2     ( HPS_FLASH_DATA[2]    ),     
+		.hps_0_hps_io_hps_io_qspi_inst_IO3     ( HPS_FLASH_DATA[3]    ),     
+		.hps_0_hps_io_hps_io_qspi_inst_SS0     ( HPS_FLASH_NCSO    ),        
+		.hps_0_hps_io_hps_io_qspi_inst_CLK     ( HPS_FLASH_DCLK    ),        
+		.hps_0_hps_io_hps_io_sdio_inst_CMD     ( HPS_SD_CMD    ),           
+		.hps_0_hps_io_hps_io_sdio_inst_D0      ( HPS_SD_DATA[0]     ),      
+		.hps_0_hps_io_hps_io_sdio_inst_D1      ( HPS_SD_DATA[1]     ),      
+		.hps_0_hps_io_hps_io_sdio_inst_CLK     ( HPS_SD_CLK   ),            
+		.hps_0_hps_io_hps_io_sdio_inst_D2      ( HPS_SD_DATA[2]     ),      
+		.hps_0_hps_io_hps_io_sdio_inst_D3      ( HPS_SD_DATA[3]     ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D0      ( HPS_USB_DATA[0]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D1      ( HPS_USB_DATA[1]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D2      ( HPS_USB_DATA[2]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D3      ( HPS_USB_DATA[3]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D4      ( HPS_USB_DATA[4]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D5      ( HPS_USB_DATA[5]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D6      ( HPS_USB_DATA[6]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_D7      ( HPS_USB_DATA[7]    ),      
+		.hps_0_hps_io_hps_io_usb1_inst_CLK     ( HPS_USB_CLKOUT    ),       
+		.hps_0_hps_io_hps_io_usb1_inst_STP     ( HPS_USB_STP    ),          
+		.hps_0_hps_io_hps_io_usb1_inst_DIR     ( HPS_USB_DIR    ),          
+		.hps_0_hps_io_hps_io_usb1_inst_NXT     ( HPS_USB_NXT    ),          
+		.hps_0_hps_io_hps_io_spim0_inst_CLK    ( HPS_LCM_SPIM_CLK),    
+		.hps_0_hps_io_hps_io_spim0_inst_MOSI   ( HPS_LCM_SPIM_MOSI),   
+		.hps_0_hps_io_hps_io_spim0_inst_MISO   ( HPS_LCM_SPIM_MISO),   
+		.hps_0_hps_io_hps_io_spim0_inst_SS0    ( HPS_LCM_SPIM_SS),    
+		.hps_0_hps_io_hps_io_spim1_inst_CLK    ( HPS_SPIM_CLK  ),           
+		.hps_0_hps_io_hps_io_spim1_inst_MOSI   ( HPS_SPIM_MOSI ),           
+		.hps_0_hps_io_hps_io_spim1_inst_MISO   ( HPS_SPIM_MISO ),           
+		.hps_0_hps_io_hps_io_spim1_inst_SS0    ( HPS_SPIM_SS ),             
+		.hps_0_hps_io_hps_io_uart0_inst_RX     ( HPS_UART_RX    ),          
+		.hps_0_hps_io_hps_io_uart0_inst_TX     ( HPS_UART_TX    ),          
+		.hps_0_hps_io_hps_io_i2c0_inst_SDA     ( HPS_I2C1_SDAT    ),        
+		.hps_0_hps_io_hps_io_i2c0_inst_SCL     ( HPS_I2C1_SCLK    ),        
+		.hps_0_hps_io_hps_io_i2c1_inst_SDA     ( HPS_I2C2_SDAT    ),        
 		.hps_0_hps_io_hps_io_i2c1_inst_SCL     ( HPS_I2C2_SCLK    ),      
-		//HPS GPIO  
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO09  ( HPS_CONV_USB_N),           //                               .hps_io_gpio_inst_GPIO09
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO35  ( HPS_ENET_INT_N),           //                               .hps_io_gpio_inst_GPIO35
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO37  ( HPS_LCM_BK ),  //                               .hps_io_gpio_inst_GPIO37
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO40  ( HPS_LTC_GPIO ),              //                               .hps_io_gpio_inst_GPIO40
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO41  ( HPS_LCM_D_C ),              //                               .hps_io_gpio_inst_GPIO41
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO44  ( HPS_LCM_RST_N  ),  //                               .hps_io_gpio_inst_GPIO44
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO48  ( HPS_I2C_CONTROL),          //                               .hps_io_gpio_inst_GPIO48
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO53  ( HPS_LED),                  //                               .hps_io_gpio_inst_GPIO53
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO54  ( HPS_KEY),                  //                               .hps_io_gpio_inst_GPIO54
-		.hps_0_hps_io_hps_io_gpio_inst_GPIO61  ( HPS_GSENSOR_INT),  //                               .hps_io_gpio_inst_GPIO61
-
-		.clk_clk                                                 (CLOCK_50),                                                 //                                             clk.clk
-		.reset_reset_n                                           (globalReset),                                           //                                           reset.reset_n
-		.receivertopqsys_0_ofdmadccontrol_external_adc_RealData  (ADA_D),  //   receivertopqsys_0_ofdmadccontrol_external_adc.RealData
-		.receivertopqsys_0_ofdmadccontrol_external_adc_ImagData  (ADB_D),  //                                                .ImagData
-		.transmittertopqsys_0_ofdmdaccontrol_dac_control_chadata (DA), // transmittertopqsys_0_ofdmdaccontrol_dac_control.chadata
-		.transmittertopqsys_0_ofdmdaccontrol_dac_control_chbdata (DB), //                                                .chbdata
-		.transmittertopqsys_0_externalspi_export_0_mosi          (GPIO[0]),          //       transmittertopqsys_0_externalspi_export_0.mosi
-		.transmittertopqsys_0_externalspi_export_0_nss           (GPIO[1]),           //                                                .nss
-		.transmittertopqsys_0_externalspi_export_0_miso          (GPIO[2]),          //                                                .miso
-		.transmittertopqsys_0_externalspi_export_0_sclk          (GPIO[3]),          //                                                .sclk
-		.dccclock_clk                                            (DCClock),                                            //                                        dccclock.clk
-		.signaltapclock_clk                                      (signalTapPLL),                                       //                                  signaltapclock.clk
-
-		.hps_0_h2f_reset_reset_n                                 (KEY[1])                                 //                                 hps_0_h2f_reset.reset_n
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO09  ( HPS_CONV_USB_N),           
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO35  ( HPS_ENET_INT_N),           
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO37  ( HPS_LCM_BK ),  
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO40  ( HPS_LTC_GPIO ),              
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO41  ( HPS_LCM_D_C ),              
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO44  ( HPS_LCM_RST_N  ),  
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO48  ( HPS_I2C_CONTROL),          
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO53  ( HPS_LED),                  
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO54  ( HPS_KEY),                  
+		.hps_0_hps_io_hps_io_gpio_inst_GPIO61  ( HPS_GSENSOR_INT),  
+		.clk_clk                                                 (CLOCK_50),                                                 
+		.reset_reset_n                                           (globalReset),                                           
+		.receivertopqsys_0_ofdmadccontrol_external_adc_RealData  (ADA_D),  
+		.receivertopqsys_0_ofdmadccontrol_external_adc_ImagData  (ADB_D),  
+		.transmittertopqsys_0_ofdmdaccontrol_dac_control_chadata (DA), 
+		.transmittertopqsys_0_ofdmdaccontrol_dac_control_chbdata (DB), 
+		.transmittertopqsys_0_externalspi_export_0_mosi          (GPIO[0]),          
+		.transmittertopqsys_0_externalspi_export_0_nss           (GPIO[1]),           
+		.transmittertopqsys_0_externalspi_export_0_miso          (GPIO[2]),          
+		.transmittertopqsys_0_externalspi_export_0_sclk          (GPIO[3]),          
+		.dccclock_clk                                            (DCClock),                                            
+		.signaltapclock_clk                                      (signalTapPLL),                                       
+		.hps_0_f2h_warm_reset_req_reset_n                        (KEY[1]),                                 
+		.txresetin_reset_n                                       (Hps2FPGAReset[1]),
+		.rxresetin_reset_n													(Hps2FPGAReset[0]),
+		.hps2fpgaresetpio_export                                 (Hps2FPGAReset)                                        
 	);
+
     
 endmodule
         
